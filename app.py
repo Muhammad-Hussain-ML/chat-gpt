71f4cb5e91055c9ef0a34cf737b913bf4bba7821
Muhammad-Hussain-ML <hussain.ml.engineer@gmail.com>
2025-06-19 11:34:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # --- Load environment variables ---
 load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = os.getenv("GEMINI_API_KEY")
 st.write(api_key)
 # --- Configure Google Generative AI ---
 if not api_key:
